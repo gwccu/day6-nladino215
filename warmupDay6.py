07cdf9722ba02054cd48cfffcf1772acbdad716f
Maya Lavi <nladino215@gmail.com>
2019-03-30 15:00:41 +0000

    worked on warm up

--- warmupDay6.py
+++ warmupDay6.py
@@ -1,1 +1,24 @@
 # File name: warmupDay6.py
+mynum = 2
+guess = int(input("Guess a number"))
+while guess != mynum:
+    if guess != mynum:
+        print("try again")
+        guess = int(input("Guess a number"))
+    if guess == mynum:
+        print("You got it!")
+print("good job!")
+
+n = 2
+m = 3
+print(n ** m)
+
+n = 2
+m = 3
+while n <= 2 ** m:
+    if n < 2 ** m:
+        print(n)
+        n *= n
+    if n == 2 ** m:
+        print(n)
+        print("done")
