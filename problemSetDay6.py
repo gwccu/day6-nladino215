9f4cb754b35fa176055c9b4b2d85b7e3b1cc0a98
Maya Lavi <nladino215@gmail.com>
2019-03-30 16:13:41 +0000

    worked on packet

--- problemSetDay6.py
+++ problemSetDay6.py
@@ -1,1 +1,16 @@
 # File name: problemSetDay6.py
+num = 1
+n = int(input("write a number"))
+while num <= n:
+    if num % 2 == 0:
+        print("# is even")
+        num += 1
+    if num % 2 != 0:
+        print("# is odd")
+        num += 1
+t = "*"
+print(t)
+while t != "****":
+    if t != "****":
+        t += "*"
+        print(t)
